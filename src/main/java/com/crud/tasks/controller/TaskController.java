5b7570681bb2d2eab949c5b9fd4a3ef02a2427bf
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-03-29 15:12:15 +0200

    18.4 Added Controller methods mapping.

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,32 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+    @GetMapping(value = "getTask")
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "test title", "test content");
+    }
+
+    @DeleteMapping(value = "deleteTask") public void deleteTask(Long taskId) {
+    }
+
+    @PutMapping(value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "edited title", "test_content");
+    }
+
+    @PostMapping(value = "createTask") public void createTask(TaskDto taskDto) {
+    }
+}
