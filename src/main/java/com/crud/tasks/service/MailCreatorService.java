9f83925c9da45b941e7f9162cb6085b0e7dfbc56
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-07-08 14:55:28 +0200

    32.3 Added advanced functions of Thymeleaf library.

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -22,6 +25,11 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://laperacarlos.github.io/");
@@ -29,6 +37,10 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName() + ",");
         context.setVariable("goodbye_msg", "Thank you for being with us!");
         context.setVariable("cmp_details", companyConfig.getName()+ ", phone: " + companyConfig.getPhone() + ", email: " + companyConfig.getEmail() + ". " + companyConfig.getGoal());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
